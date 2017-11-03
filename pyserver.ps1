ec63cb00a997e7d1ffe7398623ab22b2d50c0728
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-03 14:20:28 -0700

    Now handles error cases and has a help command

--- pyserver.ps1
+++ pyserver.ps1
@@ -2,7 +2,7 @@ param(
     [string]$type,
     [string]$title
 )
-
+$ErrorActionPreference = "Stop"
 $html = @"
 <!DOCTYPE html>
 <html lang="en">
@@ -29,21 +29,48 @@ def index():
 server.run(debug=True)
 "@
 
-if($type -eq $null -and $title -eq $null){
-    Write-Error "command: type does not exist. Must contain either -type init or -title <name>".
+$helpTxt = @"
+
+pyserver -type <types> [-title <titles>]
+
+types available:
+    init: loads default flask file in current directory only
+    help: loads list of commands in commandline
+
+titles available:
+    [any]: When using -title command it creates a directory under that name and loads all default flask files
+           if type is not set, init is the default
+
+"@
+if ($title){
+    try{
+        Write-Host "Building..." -NoNewline
+        mkdir $title > $null
+        mkdir $title\templates, $title\static > $null
+        mkdir $title\static\css, $title\static\images, $title\static\scripts > $null
+        New-Item $title\server.py, $title\static\css\styles.css, $title\templates\index.html,$title\static\scripts\app.js > $null
+        $html | Set-Content $title'\templates\index.html' > $null
+        $py | Set-Content $title'\server.py' > $null
+        Write-Host " Build finished! All filed loaded successfully."
+    }catch{
+        Write-Host " Build Failed! Some files may already exist! To avoid overwriting, those files were skipped"
+    }
 }
-elseif($type -eq "init"){
-    mkdir templates, static
-    mkdir static\css, static\images, static\scripts
-    New-Item server.py, static\css\styles.css, templates\index.html, static\scripts\app.js
-    $html | Set-Content 'templates\index.html'
-    $py | Set-Content 'server.py'
+elseif($type.equals("init")){
+    try{
+        Write-Host "Building... " -NoNewline
+        mkdir templates, static
+        mkdir static\css, static\images, static\scripts
+        New-Item server.py, static\css\styles.css, templates\index.html, static\scripts\app.js
+        $html | Set-Content 'templates\index.html'
+        $py | Set-Content 'server.py'
+    }catch{
+        Write-Host " Build Failed! Some files may already exist! To avoid overwriting, those files were skipped"
+    }
 }
-elseif ($title -ne $null){
-    mkdir $title
-    mkdir $title\templates, $title\static
-    mkdir $title\static\css, $title\static\images, $title\static\scripts
-    New-Item $title\server.py, $title\static\css\styles.css, $title\templates\index.html,$title\static\scripts\app.js
-    $html | Set-Content $title'\templates\index.html'
-    $py | Set-Content $title'\server.py'
+elseif($type.equals("help")){
+    Write-Host $helpTxt
+}
+else{
+    Write-Host "command type does not exist. Must contain either -type <types> or -title <titles>. Write pyserver help to look at commands list"
 }
